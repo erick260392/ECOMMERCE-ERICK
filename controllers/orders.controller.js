a9e82f4d12a30870eb87e12820bc5a7ac811e793
erick260392 <erickliconamorales2016@gmail.com>
2022-10-06 10:42:58 -0500

    finaly

--- controllers/orders.controller.js
+++ controllers/orders.controller.js
@@ -106,33 +106,80 @@ const updateProductInCart = catchAsync(async (req, res, next) => {
 const purchaseCart = catchAsync(async (req, res, next) => {
   const { sessionUser } = req;
 
-  const id = sessionUser.id;
+  const cart = await Cart.findOne({
+    where: { status: 'active', userId: sessionUser.id },
+    include: {
+      model: ProductInCart,
+      status: 'active',
+      include: { model: Product },
+    },
+  });
 
-  const purchase = await Cart.findOne({
-    where: { userid: id, status: 'active' },
-    include: [
-      {
-        model: Product,
-        required: false, // Apply OUTER JOIN
-        where: { status: 'active' },
-     
-      },
-    ],
+  if (!cart) {
+    return next(new AppError('This user does not have a cart', 400));
+  }
+
+  // Loop through the products in the cart
+  let totalPrice = 0;
+  const cartPromises = cart.productInCarts.map(async productInCart => {
+    // Update product to status purchased
+    await productInCart.update({ status: 'purchased' });
+
+    // Calculate total price
+    const productPrice = productInCart.product.price * productInCart.quantity;
+
+    totalPrice += productPrice;
+
+    // Substract requested qty to product's current qty
+    const newQty = productInCart.product.quantity - productInCart.quantity;
+
+    await productInCart.product.update({ quantity: newQty });
+  });
+
+  await Promise.all(cartPromises);
+
+  await cart.update({ status: 'purchased' });
+
+  const newOrder = await Order.create({
+    cartId: cart.id,
+    userId: sessionUser.id,
+    totalPrice,
+  });
+
+  res.status(201).json({
+    status: 'success',
+    data: { newOrder },
   });
 
 
 });
 
 const removeProductFromCart = catchAsync(async (req, res) => {
-  const { id } = req.params;
 
-  const removeproduct = await ProductInCart.findOne({
-    where: { productId: id, status: 'active' },
+  const { sessionUser } = req;
+  const { productId } = req.params;
+
+  const cart = await Cart.findOne({
+    where: { userId: sessionUser.id, status: 'active' },
   });
 
-  await removeproduct.update({ quantity: 0, status: 'removed' });
+  if (!cart) {
+    return next(new AppError('This user does not have a cart', 400));
+  }
 
-  res.status(204).json({ status: 'success' });
+  const productInCart = await ProductInCart.findOne({
+    where: { cartId: cart.id, productId, status: 'active' },
+  });
+
+  if (!productInCart) {
+    return next(new AppError('This product is not in your cart', 400));
+  }
+
+  await productInCart.update({ quantity: 0, status: 'removed' });
+
+  res.status(200).json({
+    status: 'success',
+  });
 });
 
 module.exports = {
